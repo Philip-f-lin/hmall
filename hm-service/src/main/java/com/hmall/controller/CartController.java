1f5ada7f8bf4cef9258ebfd2ce3b3424bb110994
linzhuofei <philip.f.lin@gmail.com>
2025-03-29 17:14:14 +0800

    文字修正

--- hm-service/src/main/java/com/hmall/controller/CartController.java
+++ hm-service/src/main/java/com/hmall/controller/CartController.java
@@ -15,38 +15,38 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@Api(tags = "购物车相关接口")
+@Api(tags = "購物車相關介面")
 @RestController
 @RequestMapping("/carts")
 @RequiredArgsConstructor
 public class CartController {
     private final ICartService cartService;
 
-    @ApiOperation("添加商品到购物车")
+    @ApiOperation("加入商品到購物車")
     @PostMapping
     public void addItem2Cart(@Valid @RequestBody CartFormDTO cartFormDTO){
         cartService.addItem2Cart(cartFormDTO);
     }
 
-    @ApiOperation("更新购物车数据")
+    @ApiOperation("更新購物車數據")
     @PutMapping
     public void updateCart(@RequestBody Cart cart){
         cartService.updateById(cart);
     }
 
-    @ApiOperation("删除购物车中商品")
+    @ApiOperation("刪除購物車中商品")
     @DeleteMapping("{id}")
-    public void deleteCartItem(@Param ("购物车条目id")@PathVariable("id") Long id){
+    public void deleteCartItem(@Param ("購物車條目id")@PathVariable("id") Long id){
         cartService.removeById(id);
     }
 
-    @ApiOperation("查询购物车列表")
+    @ApiOperation("查詢購物車列表")
     @GetMapping
     public List<CartVO> queryMyCarts(){
         return cartService.queryMyCarts();
     }
-    @ApiOperation("批量删除购物车中商品")
-    @ApiImplicitParam(name = "ids", value = "购物车条目id集合")
+    @ApiOperation("批量刪除購物車中商品")
+    @ApiImplicitParam(name = "ids", value = "購物車條目id集合")
     @DeleteMapping
     public void deleteCartItemByIds(@RequestParam("ids") List<Long> ids){
         cartService.removeByItemIds(ids);
