9a4fcd54124f737d83826ac147494b716b4018e7
linzhuofei <philip.f.lin@gmail.com>
2025-04-13 16:22:28 +0800

    微服務 - 微服務拆分 - 訂單服務

---
+++ order-service/src/main/java/com/hmall/order/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.hmall.order.controller;
+
+import com.hmall.common.utils.BeanUtils;
+import com.hmall.order.domain.dto.OrderFormDTO;
+import com.hmall.order.domain.vo.OrderVO;
+import com.hmall.order.service.IOrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = "訂單管理介面")
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final IOrderService orderService;
+
+    @ApiOperation("根據id查詢訂單")
+    @GetMapping("{id}")
+    public OrderVO queryOrderById(@Param ("訂單id")@PathVariable("id") Long orderId) {
+        return BeanUtils.copyBean(orderService.getById(orderId), OrderVO.class);
+    }
+
+    @ApiOperation("建立訂單")
+    @PostMapping
+    public Long createOrder(@RequestBody OrderFormDTO orderFormDTO){
+        return orderService.createOrder(orderFormDTO);
+    }
+
+    @ApiOperation("標記訂單已支付")
+    @ApiImplicitParam(name = "orderId", value = "訂單id", paramType = "path")
+    @PutMapping("/{orderId}")
+    public void markOrderPaySuccess(@PathVariable("orderId") Long orderId) {
+        orderService.markOrderPaySuccess(orderId);
+    }
+}
---
+++ order-service/src/main/java/com/hmall/order/domain/dto/OrderDetailDTO.java
@@ -0,0 +1,16 @@
+package com.hmall.order.domain.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@ApiModel(description = "訂單明細條")
+@Data
+@Accessors(chain = true)
+public class OrderDetailDTO {
+    @ApiModelProperty("商品id")
+    private Long itemId;
+    @ApiModelProperty("商品購買數量")
+    private Integer num;
+}
