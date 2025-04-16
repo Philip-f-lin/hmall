3b868e37db5ec67fb0a3dc169819b065fe9643f2
linzhuofei <philip.f.lin@gmail.com>
2025-04-16 22:33:40 +0800

    微服務 - 微服務拆分 - 使用者微服務

---
+++ user-service/src/main/java/com/hmall/user/domain/dto/LoginFormDTO.java
@@ -0,0 +1,20 @@
+package com.hmall.user.domain.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@ApiModel(description = "登入表單實體")
+public class LoginFormDTO {
+    @ApiModelProperty(value = "使用者名稱", required = true)
+    @NotNull(message = "使用者名稱不能為空")
+    private String username;
+    @NotNull(message = "密碼不能為空")
+    @ApiModelProperty(value = "使用者名稱", required = true)
+    private String password;
+    @ApiModelProperty(value = "是否记住我", required = false)
+    private Boolean rememberMe = false;
+}
