c85e6127d749b8688b761a8aa917a74c9c671144
linzhuofei <philip.f.lin@gmail.com>
2025-04-22 08:29:16 +0800

    微服務 - 微服務拆分 - 使用者微服務

---
+++ user-service/src/main/java/com/hmall/user/service/IAddressService.java
@@ -0,0 +1,13 @@
+package com.hmall.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmall.user.domain.po.Address;
+
+/**
+ * <p>
+ *  服務類
+ * </p>
+ */
+public interface IAddressService extends IService<Address> {
+
+}
